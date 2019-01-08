b506059d5f178f143217d8240fa3f5749c25a09a
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2019-01-08 20:52:53 +0100

    draw a complete cube from its 6 surfaces!

--- simpleplane/MeshInstance.gd
+++ simpleplane/MeshInstance.gd
@@ -13,26 +13,50 @@ func _ready():
 	var size = 2;
 	var surfaceTool = SurfaceTool.new();
 	
+	var uv00 = Vector2(0,0)
+	var uv10 = Vector2(1,0)
+	var uv01 = Vector2(0,1)
+	var uv11 = Vector2(1,1)
+	
+	# piano di sotto 
+	var p0 = Vector3( -0.5, -0.5, 0.5)
+	var p1 = Vector3( 0.5, -0.5, 0.5)
+	var p2 = Vector3( 0.5, -0.5, -0.5)
+	var p3 = Vector3( -0.5, -0.5, -0.5)
+	
+	# piano di sopra
+	var p4 = Vector3( -0.5, 0.5, 0.5)
+	var p5 = Vector3( 0.5,0.5,0.5)
+	var p6 = Vector3( 0.5,0.5,-0.5)
+	var p7 = Vector3( -0.5, 0.5, -0.5)
+	
 	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	surfaceTool.set_material(mat)
-	surfaceTool.add_uv(Vector2(1, 1));
-	surfaceTool.add_vertex(Vector3(-size, -size,  0))
-	surfaceTool.add_uv(Vector2(0, 0));
-	surfaceTool.add_vertex(Vector3( size,  size,  0))
-	surfaceTool.add_uv(Vector2(0, 1));
-	surfaceTool.add_vertex(Vector3( size, -size,  0))
-	surfaceTool.add_uv(Vector2(1, 1));
-	surfaceTool.add_vertex(Vector3(-size, -size,  0))
-	surfaceTool.add_uv(Vector2(1, 0));
-	surfaceTool.add_vertex(Vector3(-size,  size,  0))
-	surfaceTool.add_uv(Vector2(0, 0));
-	surfaceTool.add_vertex(Vector3( size,  size,  0))
+	var uvs = [ uv11, uv00, uv01, uv11, uv10, uv00 ]
+	# +z
+	plane(surfaceTool, uvs, [ p0, p5, p1, p0, p4, p5])
+	# +y
+	plane(surfaceTool, uvs,	[ p4, p6, p5, p4, p7, p6])
+	# -x
+	plane(surfaceTool, uvs, [ p0, p7, p4, p0, p3, p7])
+	# -y
+	plane(surfaceTool, uvs, [ p3, p2, p6, p3, p6, p7])
+	# +x
+	plane(surfaceTool, uvs, [ p6, p1, p5, p6, p2, p1])
+	# -y
+	plane(surfaceTool, uvs, [ p1, p2, p3, p1, p3, p0])
 	surfaceTool.generate_normals()
 	
 	var new_mesh = surfaceTool.commit()
 	self.mesh = new_mesh;
 	
+func plane(surfaceTool,uvs, points):
+	for i in range(uvs.size()):
+		surfaceTool.add_uv(uvs[i])
+		surfaceTool.add_vertex(points[i])
+
+	
 	
 func _process(delta):
-    #angle += delta * 30
-    self.rotation_degrees = Vector3(0, 0, angle)
+    angle += delta * 100
+    self.rotation_degrees = Vector3(angle, angle, angle)
