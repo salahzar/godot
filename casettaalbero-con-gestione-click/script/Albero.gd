3136820d435fdd2fd0d8b011e39331ef398f24c7
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2019-03-04 00:06:52 +0100

    refactoring

--- casettaalbero-con-gestione-click/script/Albero.gd
+++ casettaalbero-con-gestione-click/script/Albero.gd
@@ -1,10 +1,13 @@
 extends StaticBody
 
-func _input_event(viewport, event, shape_idx, vector, integer):
-	if (event is InputEventMouseButton 
+func left_button(event):
+	return (event is InputEventMouseButton 
 		and event.button_index == 1 
 		and event.is_pressed() 
-		and not event.is_echo()):
+		and not event.is_echo())
+
+func _input_event(viewport, event, shape_idx, vector, integer):
+	if left_button(event):
 	    #assuming you want the object to move upwards:
 	    #self.set_linear_velocity(Vector2(0, -300))
 		print("HELLO TREE")
